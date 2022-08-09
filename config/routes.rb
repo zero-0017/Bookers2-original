a599256d037c761ad57d6a0cbdf270b4b149974c
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-08-09 14:14:51 +0000

    [応用課題３]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+  resource :favorites, only: [:create, :destroy]
+end
+
   resources :users, only: [:index,:show,:edit,:update]
+
 end
