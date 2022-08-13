c3910eb1797b4f96adde57d91a70298ee9728ddd
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-08-13 05:09:35 +0000

    [応用課題６]コメント機能　非同期通信（Finish）

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'net-smtp'
 gem 'jquery-rails'
+gem 'haml-rails'
--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,20 @@
 class BookCommentsController < ApplicationController
+
   def create
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to request.referer
+    render :create
+    # @book_comment = BookComment.new
   end
 
   # 同じ画面に戻る（redirect_to request.referer）
 
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    render :destroy
   end
 
   private
