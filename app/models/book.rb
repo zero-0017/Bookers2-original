388bf610b84ee1ea4486a2069127a7bee532f367
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-08-17 07:42:57 +0000

    [応用課題7b]投稿数表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,12 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+#scope :スコープの名前, -> { 条件式 }
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }# Time.zone.now.all_day　1日を表す。
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }# 1.day.ago.all_day　前日を表す。
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }# １週間
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }# ２週間前
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
